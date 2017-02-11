661248ed4fafbc7399c08742464e97c1e9eb44d2
Hebert Porto <hebertporto@gmail.com>
2017-02-11 15:02:40 -0200

    adionado splashscreen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Splashscreen from '@remobile/react-native-splashscreen';
 
 class App extends Component {
+
+  componentDidMount() {
+     Splashscreen.hide();
+  }
 
   render() {
      return (
