df18fa5f85f383a8939b09b88549b83f2519dd92
Hebert Porto <hebertporto@gmail.com>
2017-03-10 12:42:29 -0300

    ajuste de imagem na main screen

--- src/components/ListNovels.js
+++ src/components/ListNovels.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     width: undefined,
     height: 110,
     justifyContent: 'flex-end',
-    alignItems: 'flex-start'
+    alignItems: 'flex-start',
   },
   textStyle: {
      flex: 1,
