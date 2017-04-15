5cfd50a4945faac628b5973e1e29a91dd12c45b9
Hebert Porto <hebertporto@gmail.com>
2017-04-14 22:03:10 -0300

    added search on main screen

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import { ListView } from 'react-native';
+import { ListView, RefreshControl, TextInput } from 'react-native';
 import { connect } from 'react-redux';
 import ListNovels from './ListNovels';
 import { novelsFetch } from '../actions';
 
 class MainScreen extends Component {
-
-  componentWillMount() {
-    this.props.novelsFetch();
-    this.createDataSource(this.props);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.createDataSource(nextProps);
-  }
-
-  createDataSource({ novels }) {
-    const ds = new ListView.DataSource({
+  
+   constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: false,
+      dataSource: new ListView.DataSource({
         rowHasChanged: (r1, r2) => r1 !== r2
+      }),
+      text: '',
+      novels: []
+    };
+    this.props.novelsFetch();
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    this.setState({ novels: nextProps.novels });
+  }
+
+  async onRefresh() {
+    this.setState({ 
+      refreshing: true,
+      text: ''
+     });
+    await this.props.novelsFetch();    
+    this.setState({ refreshing: false });
+  }
+
+  filterSearch(text) {
+    const { novels } = this.props;
+    const newData = novels.filter((item) => {
+      const itemData = item.name.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
     });
-    this.dataSource = ds.cloneWithRows(novels);
+
+    this.setState({
+      novels: newData,
+      text
+    });
+  }
+  
+  searchInput() {
+    return (
+            <TextInput
+              onChangeText={(text) => this.filterSearch(text)}
+              placeholder="Pesquisar"
+              value={this.state.text}
+            />
+        );
   }
 
   renderRow(novel) {
@@ -27,11 +61,23 @@ class MainScreen extends Component {
   }
 
   render() {
+    const ds = this.state.dataSource;
+    const { novels } = this.state;
+    
     return (
       <ListView
         enableEmptySections
-        dataSource={this.dataSource}
+        dataSource={ds.cloneWithRows(novels)}
         renderRow={this.renderRow}
+        refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this.onRefresh.bind(this)}
+              colors={['#ff0000', '#00ff00', '#0000ff']}
+              tintColor="#00ff00"
+            />
+          }
+        renderHeader={this.searchInput.bind(this)}
       />
     );
   }
